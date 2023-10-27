d557e0ba9d76855a180af752536b3c62eb389e50
Santienper <stienda@ucm.es>
2023-10-27 10:03:23 +0200

    :)

--- skeleton/Objects/ParticleGenerators/ParticleGen.cpp
+++ skeleton/Objects/ParticleGenerators/ParticleGen.cpp
@@ -2,7 +2,7 @@
 #include "Structure/Scene.h"
 
 ParticleGen::ParticleGen(const Vector3& pos, double rate) : Object(pos), time(0), rate(rate) {
-	sys = static_cast<ParticleSystem*>(Scene::get()->getSystem(ids::sys_particle));
+	sys = static_cast<ParticleSystem*>(Scene::get()->getSystem("particles"));
 	if(sys == nullptr) throw std::exception("No hay sistema de partículas");
 }
 
